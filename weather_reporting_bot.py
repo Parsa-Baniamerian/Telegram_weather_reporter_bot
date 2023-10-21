240fc7a757dd3ec37bcbc789241f929c6d186630
ParsaBaniamerian <parsabaniamerian03@gmail.com>
2023-10-21 20:09:43 +0330

    message handler's bug fixed. main function and error printer and error handler were added too!

--- weather_reporting_bot.py
+++ weather_reporting_bot.py
@@ -9,9 +9,8 @@ BOT_USERNAME: Final = "@Weather_Land_Bot"
 BASE_URL: Final = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY: Final = "8438a0ea83aa941f5c4f746f15f95c26"
 
+
 # Commands
-
-
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hi! I am a meteorological robot")
 
@@ -20,33 +19,57 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("If you add me to a group, I will send a text message about the weather in Tehran every ten minutes. If you want to know about the weather condition of another city, you can send me the name of the city you want in P.V., or mention me in a message in the group and write the name of that city :)")
 
 
-# Handle Message
+# Handle Messages
 async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_type: update.message.chat.type
-    city = update.message.text.strip()
+    message_type: str = update.message.chat.type
+    city: str = update.message.text.strip()
 
     if message_type == "group":
         if BOT_USERNAME in city:
             target_city = city.replace(BOT_USERNAME, "").strip()
             city = target_city
 
-    city = "New York"
     url = BASE_URL + "appid=" + API_KEY + "&q=" + city
     response = requests.get(url).json()
 
     def kelvin_to_celsius(kelvin): return kelvin - 273.15
     temp_kelvin = response["main"]["temp"]
-    temp_celsius = kelvin_to_celsius(temp_kelvin)
+    temp_celsius = str(int(kelvin_to_celsius(temp_kelvin)))
 
     feels_like_kelvin = response["main"]["feels_like"]
-    feels_like_celsius = kelvin_to_celsius(feels_like_kelvin)
+    feels_like_celsius = str(int(kelvin_to_celsius(feels_like_kelvin)))
 
-    humidity = response["main"]["humidity"]
+    humidity = str(response["main"]["humidity"])
 
     description = response["weather"][0]["description"]
 
-    wind_speed = response["wind"]["speed"]
+    wind_speed = str(round(response["wind"]["speed"], 1))
 
-    result = city + "\t" + temp_celsius + "°C\t" + description + "\nHumidity: " + humidity + "\nWind speed: " + wind_speed + "km/h" + "\nFeels like: "  + feels_like_celsius +"°C"
+    result = city + "\t" + temp_celsius + "°C\t" + description + "\nHumidity: " + humidity + "%" \
+        "\nWind speed: " + wind_speed + "km/h" + \
+        "\nFeels like: " + feels_like_celsius + "°C"
     await update.message.reply_text(result)
 
+
+# Watch errors
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+
+if __name__ == "__main__":
+    print("Starting bot")
+    app = Application.builder().token(TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_messages))
+
+    # Errors
+    app.add_error_handler(error)
+
+    # Check for new messages from user
+    print("Polling...")
+    app.run_polling(poll_interval=1)
